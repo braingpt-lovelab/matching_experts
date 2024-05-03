5a51ee464841989f37bb656d10f6cbc2877e49c6
Ubuntu <ken@braingptbenchmark.ycq3ohurltmuvcsgktwuxjhoib.zrhx.internal.cloudapp.net>
2024-05-03 11:27:22 +0000

    args loaded from config

--- train.sh
+++ train.sh
@@ -1,21 +1,12 @@
-expdir=exp/gpt2
+#!/bin/bash
+
+# Define the JSON file and expdir
+args_json="configs/gpt2_test.json"
+expdir="exp/gpt2"
 mkdir -p $expdir
 
-# python 
-accelerate launch --config_file accel_config.yaml finetune.py \
-    --model_path gpt2 \
-    --data_path BrainGPT/train_valid_split_pmc_neuroscience_2002-2022_filtered_subset_mini \
-    --cache_dir cache \
-    --batch_size 1 \
-    --chunk_size 512 \
-    --eval_batch_size 16 \
-    --learning_rate 2e-5 \
-    --gradient_accumulation_steps 8 \
-    --num_train_epochs 1 \
-    --num_warmup_steps 0.03 \
-    --weight_decay 0.001 \
-    --lr_scheduler_type cosine \
-    --outputdir $expdir \
-    --logfile $expdir/log.txt \
-    --log_interval 1000 \
-    --save_interval 10000 \
+# Extract the JSON contents into Bash-friendly key-value pairs
+key_value_pairs=$(python3 -c "import json; import sys; d = json.load(open('$args_json')); sys.stdout.write(' '.join([f'--{key} {value}' for key, value in d.items()]))")
+
+# Launch the Python script with the dynamically created arguments
+accelerate launch --config_file accel_config.yaml finetune.py $key_value_pairs
